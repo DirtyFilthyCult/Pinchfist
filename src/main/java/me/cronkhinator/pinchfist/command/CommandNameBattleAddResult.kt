91b3c510009996dc7ec2dce91c66ef1c2e1d5f7d
Cronkhinator <80426100+Cronkhinator@users.noreply.github.com>
2021-03-23 18:11:21 +0100

    Update CommandNameBattleAddResult.kt
    Removed mentions in command syntax
    Changed color from cyan to red

--- src/main/java/me/cronkhinator/pinchfist/command/CommandNameBattleAddResult.kt
+++ src/main/java/me/cronkhinator/pinchfist/command/CommandNameBattleAddResult.kt
@@ -17,13 +17,8 @@ object CommandNameBattleAddResult : Command {
         val nameBattleChannel = event.guild.getTextChannelsByName(Settings.nameBattleChannel, false)[0]
         event.message.delete().queue()
 
-        val mentionedMembers = event.message.mentionedMembers
-        if(mentionedMembers[0] == null || mentionedMembers[1] == null) {
-            event.channel.sendMessage(Messages.INVALID_ARGUMENT_TYPE).queue(); return
-        }
-
-        val p1 = mentionedMembers[0].user.name
-        val p2 = mentionedMembers[1].user.name
+        val p1 = args[0]
+        val p2 = args[1]
         val scoreP1 = Integer.valueOf(args[2])
         val scoreP2 = Integer.valueOf(args[3])
         val winner: String = PinchfistUtil.getWinner(scoreP1, scoreP2, p1, p2)
@@ -31,7 +26,7 @@ object CommandNameBattleAddResult : Command {
 
         val eb = EmbedBuilder()
             .setTitle("$p1 vs $p2")
-            .setColor(Colors.CYAN.hex)
+            .setColor(Colors.RED.hex)
             .setDescription("The battle for the title: $title")
             .addField(p1, scoreP1.toString(), true)
             .addField(p2, scoreP2.toString(), true)
